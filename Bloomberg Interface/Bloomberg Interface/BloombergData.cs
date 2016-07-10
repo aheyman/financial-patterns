406e3f1ccec356deb1fcb9afbd55172740563036
aheyman06 <aheyman06@gmail.com>
2016-07-10 16:50:26 -0400

    Needs a massive refactor, but we are up and running

--- Bloomberg Interface/Bloomberg Interface/BloombergData.cs
+++ Bloomberg Interface/Bloomberg Interface/BloombergData.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.IO;
+using System.Linq;
 using System.Text;
 using CorrelationID = Bloomberglp.Blpapi.CorrelationID;
 using Element = Bloomberglp.Blpapi.Element;
@@ -27,8 +29,19 @@ namespace BloombergConnection
 
     }
 
+    enum Periodcity
+    {
+        DAILY,
+        WEEKLY,
+        MONTHLY,
+        QUARTERLY,
+        YEARLY
+    }
+
     public interface Data
     {
+        DateTime StartDate { get; set; }
+        DateTime EndDate { get; set; }
         Dictionary<string, List<string>> Data { get; }
         void AddToDict(string key, string value);
         void AddToDict(string key, List<string> value);
@@ -40,15 +53,18 @@ namespace BloombergConnection
 
     public class HistoricalData : Data
     {
+        DateTime _start;
+        public DateTime StartDate { get { return _start; } set { _start = value; } }
 
+
+        DateTime _end;
+        public DateTime EndDate { get { return _end; } set { _end = value; } }
         public string TypeOfRequest { get { return RequestType.Historical.Value; } }
 
         Dictionary<string, List<string>> _dict = new Dictionary<string, List<string>>()
         {
             {"securities", new List<string>() },
             {"fields", new List<string>() },
-            {"startDate", new List<string>() { "20160101" } },
-            {"endDate", new List<string>() {"20160101" } },
             {"periodicitySelection", new List<string>() {"DAILY"} },
             {"nonTradingDayFillOption", new List<string>( ){"ACTIVE_DAYS_ONLY"}  },
         };
@@ -83,6 +99,12 @@ namespace BloombergConnection
 
     public class Reference : Data
     {
+        DateTime _start;
+        public DateTime StartDate { get { return _start; } set { _start = value; } }
+
+
+        DateTime _end;
+        public DateTime EndDate { get { return _end; } set { _end = value; } }
 
         public string TypeOfRequest { get { return RequestType.Reference.Value; } }
 
@@ -137,6 +159,7 @@ namespace BloombergConnection
 
         const string refData = @"//blp/refdata";
         const string apiFields = @"//blp/apiflds";
+        string output;
         string logFile;
 
 
@@ -149,6 +172,7 @@ namespace BloombergConnection
                 Directory.CreateDirectory(filePath);
 
             logFile = filePath + "log_" + DateTime.Now.ToShortDateString().Replace('/', '-') + ".txt";
+            output = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + @"\request_" + DateTime.Now.ToShortDateString().Replace('/', '-') + ".txt";
         }
 
 
@@ -159,6 +183,13 @@ namespace BloombergConnection
             Session session = StartSession();
             Service refDataSvc = session.GetService(refData);
             request = refDataSvc.CreateRequest(formattedData.TypeOfRequest);
+
+            DataTable table = new DataTable();
+            table.Columns.Add("date");
+            table.Columns.Add("security");
+
+            foreach (string str in formattedData.Data["fields"])
+                table.Columns.Add(str);
 
             string[] standards = { "securities", "fields" };
 
@@ -174,19 +205,48 @@ namespace BloombergConnection
                 case "HistoricalDataRequest":
 
                     ProcessHistoricalRequest(request, formattedData);
+                    session.SendRequest(request, new CorrelationID(1));
+                    ConsumeHistSession(session, table);
 
                     break;
 
                 case "ReferenceDataRequest":
 
-                    ProcessReferenceRequest(request, formattedData);
 
+                    List<string> daysToOverride = GetDateRange(formattedData.StartDate, formattedData.EndDate, Periodcity.QUARTERLY);
+
+
+                    foreach (string str in daysToOverride)
+                    {
+                        try
+                        {
+                            Request req;
+                            Session sess = StartSession();
+                            Service refdata = sess.GetService(refData);
+                            req = refdata.CreateRequest(formattedData.TypeOfRequest);
+                            ProcessReferenceRequest(request, formattedData);
+                            SetOverrides(request, new Overrides("FUNDAMENTAL_PUBLIC_DATE", str));
+                            session.SendRequest(request, new CorrelationID(1));
+                            ConsumeRefSession(session, table, str);
+                        }
+                        catch (Exception e)
+                        {
+
+                        }
+                        finally
+                        {
+                            using (StreamWriter write = new StreamWriter(output))
+                            {
+                                DataTableToCSV(table, write, true);
+                            }
+                        }
+
+
+                    }
                     break;
-
             }
 
-            session.SendRequest(request, new CorrelationID(1));
-            ConsumeSession(session);
+
 
         }
 
@@ -200,14 +260,59 @@ namespace BloombergConnection
                     HistoricalSession(request, entry.Key, entry.Value.ToArray()[0]);
             }
 
+            HistoricalSession(request, "startDate", BloombergDateHelper(formattedData.StartDate));
+            HistoricalSession(request, "endDate", BloombergDateHelper(formattedData.EndDate));
+
         }
 
 
         private void ProcessReferenceRequest(Request request, Data formattedData)
         {
-
             foreach (Overrides over in formattedData.Overrides)
                 SetOverrides(request, over);
+        }
+
+        private List<string> GetDateRange(DateTime startDate, DateTime endDate, Periodcity period)
+        {
+
+            List<string> result = new List<string>();
+
+            if (endDate < startDate)
+                throw new ArgumentException("endDate must be greater than or equal to startDate");
+
+            while (startDate <= endDate)
+            {
+
+                result.Add(BloombergDateHelper(startDate));
+
+                switch (period)
+                {
+                    case Periodcity.DAILY:
+                        startDate = startDate.AddDays(1);
+                        break;
+                    case Periodcity.WEEKLY:
+                        startDate = startDate.AddDays(7);
+                        break;
+                    case Periodcity.MONTHLY:
+                        startDate = startDate.AddMonths(1);
+                        break;
+                    case Periodcity.QUARTERLY:
+                        startDate = startDate.AddMonths(3);
+                        break;
+                    case Periodcity.YEARLY:
+                        startDate = startDate.AddYears(1);
+                        break;
+                }
+
+
+            }
+
+            return result;
+        }
+
+        private string BloombergDateHelper(DateTime date)
+        {
+            return date.Year.ToString("D4") + date.Month.ToString("D2") + date.Day.ToString("D2");
         }
 
 
@@ -222,7 +327,7 @@ namespace BloombergConnection
                 Logger("Could not start session.");
                 Environment.Exit(1);
             }
-            if (!session.OpenService("refData"))
+            if (!session.OpenService(refData))
             {
                 Logger("Could not open service refData");
                 Environment.Exit(1);
@@ -232,9 +337,61 @@ namespace BloombergConnection
         }
 
 
-        private void HandleResponseEvent(Event eventObj)
+        private void HandleHistResponse(Event eventObj, DataTable table)
         {
-            StringBuilder sb = new StringBuilder();
+
+            foreach (Message message in eventObj.GetMessages()) // go through each message in the Event
+            {
+
+                //responseError
+                Element HistoricalResponse = message.AsElement;
+                if (HistoricalResponse.HasElement("responseError")) // if there is an error, quit
+                {
+                    Logger(HistoricalResponse.GetElement("responseError").GetElementAsString("message"));
+                    Logger("Error in the response");
+                    Environment.Exit(1);
+                }
+
+                //securityData
+                Element securityDataArray = HistoricalResponse.GetElement("securityData");
+
+
+                string companyName = securityDataArray.GetElementAsString("security");
+                int sequenceNumber = securityDataArray.GetElementAsInt32("sequenceNumber");
+
+                if (securityDataArray.HasElement("securityError"))
+                {
+                    Element securityError = securityDataArray.GetElement("securityError");
+                    Logger("* security =" + companyName + " : " + securityError.GetElementAsString("message"));
+                }
+                else
+                {
+                    Element fieldDataArray = securityDataArray.GetElement("fieldData");
+                    int numItems = fieldDataArray.NumValues;
+
+                    for (int i = 0; i < numItems; i++)
+                    {
+                        Element fieldData = fieldDataArray.GetValueAsElement(i);
+                        DataRow row = table.NewRow();
+                        row["security"] = companyName;
+                        for (int k = 0; k < fieldData.NumElements; k++)
+                        {
+                            Element field = fieldData.GetElement(k);
+                            row[field.Name.ToString()] = field.GetValueAsString();
+                        }
+                        table.Rows.Add(row);
+                    }
+
+
+                }
+
+            }// end of messages
+
+        }
+
+        private void HandleRefResponse(Event eventObj, DataTable table, string date)
+        {
+
             foreach (Message message in eventObj.GetMessages()) // go through each message in the Event
             {
 
@@ -247,48 +404,41 @@ namespace BloombergConnection
                     Environment.Exit(1);
                 }
 
-                //securityData
                 Element securityDataArray = ReferenceDataResponse.GetElement("securityData");
-                int numItems = securityDataArray.NumValues;
+                Element securityData = securityDataArray.GetValueAsElement(0);
 
-                for (int i = 0; i < numItems; ++i) // iterate through the list of data elents in the message
+                string companyName = securityData.GetElementAsString("security");
+
+                if (securityDataArray.HasElement("securityError"))
                 {
-                    Element securityData = securityDataArray.GetValueAsElement(i);
-                    string companyName = securityData.GetElementAsString("security");
-                    int sequenceNumber = securityData.GetElementAsInt32("sequenceNumber");
-
-                    if (securityData.HasElement("securityError"))
-                    {
-                        Element securityError = securityData.GetElement("securityError");
-                        Logger("* security =" + companyName + " : " + securityError.GetElementAsString("message"));
-                    }
-                    else
-                    {
-
-                        Element fieldData = securityData.GetElement("fieldData");
-
-                        if (fieldData.HasElement("date"))
-                        {
-                            //TODO: process historical requests
-                            //its a historical request - process acordingly
-                        }
-                        else
-                        {
-                            //TODO: process reference requests
-                            //reference data request
-                            for (int index = 0; index < fieldData.NumElements; index++)
-                            {
-
-                            }
-                        }
-
-
-
-                    }
+                    Element securityError = securityDataArray.GetElement("securityError");
+                    Logger("* security =" + companyName + " : " + securityError.GetElementAsString("message"));
                 }
-            }
+                else
+                {
+
+                    Element fieldData = securityData.GetElement("fieldData");
+
+                    DataRow row = table.NewRow();
+                    row["security"] = companyName;
+                    row["date"] = date;
+
+                    for (int j = 0; j < fieldData.NumElements; j++)
+                    {
+                        Element field = fieldData.GetElement(j);
+                        row[field.Name.ToString()] = field.GetValueAsString();
+                    }
+                    table.Rows.Add(row);
+                }
+
+
+            }// end of messages
 
         }
+
+
+
+
 
 
         private void HandleOtherEvent(Event eventObj)
@@ -328,9 +478,10 @@ namespace BloombergConnection
         }
 
 
-        private void ConsumeSession(Session ses)
+        private void ConsumeRefSession(Session ses, DataTable table, string date)
         {
             bool continueToLoop = true;
+
             while (continueToLoop)
             {
                 Event eventObj = ses.NextEvent();
@@ -338,10 +489,10 @@ namespace BloombergConnection
                 {
                     case Event.EventType.RESPONSE: // final response
                         continueToLoop = false;
-                        HandleResponseEvent(eventObj);
+                        HandleRefResponse(eventObj, table, date);
                         break;
                     case Event.EventType.PARTIAL_RESPONSE:
-                        HandleResponseEvent(eventObj);
+                        HandleRefResponse(eventObj, table, date);
                         break;
                     default:
                         HandleOtherEvent(eventObj);
@@ -350,11 +501,56 @@ namespace BloombergConnection
             }
         }
 
+        private void ConsumeHistSession(Session ses, DataTable table)
+        {
+            bool continueToLoop = true;
+
+            while (continueToLoop)
+            {
+                Event eventObj = ses.NextEvent();
+                switch (eventObj.Type)
+                {
+                    case Event.EventType.RESPONSE: // final response
+                        continueToLoop = false;
+                        HandleHistResponse(eventObj, table);
+                        break;
+                    case Event.EventType.PARTIAL_RESPONSE:
+                        HandleHistResponse(eventObj, table);
+                        break;
+                    default:
+                        HandleOtherEvent(eventObj);
+                        break;
+                }
+            }
+        }
+
+        public bool DataTableToCSV(DataTable dtSource, StreamWriter writer, bool includeHeader)
+        {
+            if (dtSource == null || writer == null) return false;
+
+            if (includeHeader)
+            {
+                string[] columnNames = dtSource.Columns.Cast<DataColumn>().Select(column => "\"" + column.ColumnName.Replace("\"", "\"\"") + "\"").ToArray<string>();
+                writer.WriteLine(string.Join(",", columnNames));
+                writer.Flush();
+            }
+
+            foreach (DataRow row in dtSource.Rows)
+            {
+                string[] fields = row.ItemArray.Select(field => "\"" + field.ToString().Replace("\"", "\"\"") + "\"").ToArray<string>();
+                writer.WriteLine(string.Join(",", fields));
+                writer.Flush();
+            }
+
+            return true;
+        }
+
+
         public void Logger(string lines)
         {
             using (StreamWriter file = new StreamWriter(logFile, true))
             {
-                file.Write("LOG " + DateTime.Now.ToShortTimeString() + " : " + lines);
+                file.Write("LOG " + DateTime.Now.ToShortTimeString() + " : " + lines + "\n");
             }
         }
 
