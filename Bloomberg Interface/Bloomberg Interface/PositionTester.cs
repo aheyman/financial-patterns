040a71ebf4be7553a5fe26db0152c5f3538159f3
aheyman <aheyman06@gmail.com>
2016-10-18 23:56:09 -0400

    restructuring/adding testing framework

---
+++ Bloomberg Interface/Bloomberg Interface/PositionTester.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bloomberg_Interface
+{
+    class PositionTester
+    {
+
+        /*
+            which securit(ies)?
+            when did you hold it?
+            which direction?
+
+            SECURITY ..... DIRECTION ..... Cost .... Date ....... 1-mth/Price ........ 3-mth/Price ......... 6-mth/Price ....... 12-mth/Price ...... 24-mth/Price
+            AAPL US EQUITY Buy/sell         ####    01/01/2013    %%%                   %%%                       %%%               %%%               %%%               
+            .
+            .
+            .
+            .
+            .
+            .
+            BENCHMARK       Buy          ORIGINAL  Start date   blah                    blah                    blah                blah                blah
+
+        */
+
+    }
+}
