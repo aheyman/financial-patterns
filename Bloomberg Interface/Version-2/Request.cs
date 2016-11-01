fd898d1a8327d93473d85421dfb7f51d76da0729
aheyman <aheyman06@gmail.com>
2016-11-01 00:03:55 -0400

    cleaning up

---
+++ Bloomberg Interface/Version-2/Request.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Version_2
+{
+
+    public enum Periodcity
+    {
+        DAILY,
+        WEEKLY,
+        MONTHLY,
+        QUARTERLY,
+        YEARLY
+    }
+
+    class Request
+    {
+
+        string StartDate { get; set; }
+        string EndDate { get; set; }
+        string RequestType { get; set; }
+        Periodcity Period { get; set; }
+        List<string> Securities;
+        List<string> Fields;
+        List<Tuple<string, string>> Overrides;
+
+
+
+    }
+}
